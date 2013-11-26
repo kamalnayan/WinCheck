484db5edeb63bf45fad6c2706ebccfec0e70ebee
kamal nayan deo <info.kndeo@gmail.com>
2013-11-26 16:03:32 +0530

    olol

---
+++ js/index.js
@@ -0,0 +1,327 @@
+
+function toggle() {
+    var str1="key";
+    var str2=localStorage.length;
+    var key=str1.concat(str2);
+    var val1="<img src='images/car.png'>#  #";
+    var val2=document.getElementById('desc').value;
+    var val3="# Latitude: #";
+    var val4=document.getElementById('lat').value;
+    var val5="#, Longitude: #";
+    var val6=document.getElementById('lon').value;
+    var val7="#, Taged Time: #";
+    var currentdate = new Date();
+    var val8 =  currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+    var val9="#images/car.png#"; 
+   
+    var val10=document.getElementById('pos').value;
+    var value=val1.concat(val2,val3,val4,val5,val6,val7,val8,val9,val10);
+	localStorage.setItem(key,value); 
+    window.location.href = "index.html";
+     
+       
+} 
+function toggle1() {
+    
+	var str1="key";
+    var str2=localStorage.length;
+    var key=str1.concat(str2);
+    var val1="<img src='images/house.png'>#  #";
+    var val2=document.getElementById('desc').value;
+    var val3="# Latitude: #";
+    var val4=document.getElementById('lat').value;
+    var val5="#, Longitude: #";
+    var val6=document.getElementById('lon').value;
+    var val7="#, Taged Time: #";
+    var currentdate = new Date();
+    var val8 =  currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+    var val9="#images/house.png#"; 
+   
+    var val10=document.getElementById('pos').value;
+    var value=val1.concat(val2,val3,val4,val5,val6,val7,val8,val9,val10);
+	localStorage.setItem(key,value); 
+    window.location.href = "index.html";
+} 
+function toggle2() {
+    
+	var str1="key";
+    var str2=localStorage.length;
+    var key=str1.concat(str2);
+    var val1="<img src='images/supermarket.png'>#  #";
+    var val2=document.getElementById('desc').value;
+    var val3="# Latitude: #";
+    var val4=document.getElementById('lat').value;
+    var val5="#, Longitude: #";
+    var val6=document.getElementById('lon').value;
+    var val7="#, Taged Time: #";
+    var currentdate = new Date();
+    var val8 =  currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+    var val9="#images/supermarket.png#"; 
+   
+    var val10=document.getElementById('pos').value;
+    var value=val1.concat(val2,val3,val4,val5,val6,val7,val8,val9,val10);
+	localStorage.setItem(key,value); 
+    window.location.href = "index.html";
+} 
+function toggle3() {
+    
+	var str1="key";
+    var str2=localStorage.length;
+    var key=str1.concat(str2);
+    var val1="<img src='images/atm-2.png'>#  #";
+    var val2=document.getElementById('desc').value;
+    var val3="# Latitude: #";
+    var val4=document.getElementById('lat').value;
+    var val5="#, Longitude: #";
+    var val6=document.getElementById('lon').value;
+    var val7="#, Taged Time: #";
+    var currentdate = new Date();
+    var val8 =  currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+    var val9="#images/atm-2.png#"; 
+   
+    var val10=document.getElementById('pos').value;
+    var value=val1.concat(val2,val3,val4,val5,val6,val7,val8,val9,val10);
+	localStorage.setItem(key,value); 
+    window.location.href = "index.html";
+} 
+function toggle4() {
+    
+	var str1="key";
+    var str2=localStorage.length;
+    var key=str1.concat(str2);
+    var val1="<img src='images/house.png'>#  #";
+    var val2=document.getElementById('desc').value;
+    var val3="# Latitude: #";
+    var val4=document.getElementById('lat').value;
+    var val5="#, Longitude: #";
+    var val6=document.getElementById('lon').value;
+    var val7="#, Taged Time: #";
+    var currentdate = new Date();
+    var val8 =  currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+    var val9="#images/house.png#"; 
+   
+    var val10=document.getElementById('pos').value;
+    var value=val1.concat(val2,val3,val4,val5,val6,val7,val8,val9,val10);
+	localStorage.setItem(key,value); 
+    window.location.href = "index.html";
+} 
+function toggle5() {
+    
+	var str1="key";
+    var str2=localStorage.length;
+    var key=str1.concat(str2);
+    var val1="<img src='images/bar.png'>#  #";
+    var val2=document.getElementById('desc').value;
+    var val3="# Latitude: #";
+    var val4=document.getElementById('lat').value;
+    var val5="#, Longitude: #";
+    var val6=document.getElementById('lon').value;
+    var val7="#, Taged Time: #";
+    var currentdate = new Date();
+    var val8 =  currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+    var val9="#images/bar.png#"; 
+   
+    var val10=document.getElementById('pos').value;
+    var value=val1.concat(val2,val3,val4,val5,val6,val7,val8,val9,val10);
+	localStorage.setItem(key,value); 
+    window.location.href = "index.html";
+} 
+ function toggle6() {
+    
+	var str1="key";
+    var str2=localStorage.length;
+    var key=str1.concat(str2);
+    var val1="<img src='images/bus.png'>#  #";
+    var val2=document.getElementById('desc').value;
+    var val3="# Latitude: #";
+    var val4=document.getElementById('lat').value;
+    var val5="#, Longitude: #";
+    var val6=document.getElementById('lon').value;
+    var val7="#, Taged Time: #";
+    var currentdate = new Date();
+    var val8 =  currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+    var val9="#images/bus.png#"; 
+   
+    var val10=document.getElementById('pos').value;
+    var value=val1.concat(val2,val3,val4,val5,val6,val7,val8,val9,val10);
+	localStorage.setItem(key,value); 
+    window.location.href = "index.html";
+} 
+ function clearStorage(){
+        localStorage.clear();
+        window.location.href = "index.html";
+        
+    }
+function toggle7() {
+    
+	var str1="key";
+    var str2=localStorage.length;
+    var key=str1.concat(str2);
+    var val1="<img src='images/highway.png'>#  #";
+    var val2=document.getElementById('desc').value;
+    var val3="# Latitude: #";
+    var val4=document.getElementById('lat').value;
+    var val5="#, Longitude: #";
+    var val6=document.getElementById('lon').value;
+    var val7="#, Taged Time: #";
+    var currentdate = new Date();
+    var val8 =  currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+    var val9="#images/highway.png#"; 
+   
+    var val10=document.getElementById('pos').value;
+    var value=val1.concat(val2,val3,val4,val5,val6,val7,val8,val9,val10);
+	localStorage.setItem(key,value); 
+    window.location.href = "index.html";
+} 
+ 
+    function toggle8() {
+    
+	var str1="key";
+    var str2=localStorage.length;
+    var key=str1.concat(str2);
+    var val1="<img src='images/busstop.png'>#  #";
+    var val2=document.getElementById('desc').value;
+    var val3="# Latitude: #";
+    var val4=document.getElementById('lat').value;
+    var val5="#, Longitude: #";
+    var val6=document.getElementById('lon').value;
+    var val7="#, Taged Time: #";
+    var currentdate = new Date();
+    var val8 =  currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+    var val9="#images/busstop.png#"; 
+   
+    var val10=document.getElementById('pos').value;
+    var value=val1.concat(val2,val3,val4,val5,val6,val7,val8,val9,val10);
+	localStorage.setItem(key,value); 
+    window.location.href = "index.html";
+} 
+ 
+    function toggle9() {
+    
+	var str1="key";
+    var str2=localStorage.length;
+    var key=str1.concat(str2);
+    var val1="<img src='images/busstop.png'>#  #";
+    var val2=document.getElementById('desc').value;
+    var val3="# Latitude: #";
+    var val4=document.getElementById('lat').value;
+    var val5="#, Longitude: #";
+    var val6=document.getElementById('lon').value;
+    var val7="#, Taged Time: #";
+    var currentdate = new Date();
+    var val8 =  currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+    var val9="#images/busstop.png#"; 
+   
+    var val10=document.getElementById('pos').value;
+    var value=val1.concat(val2,val3,val4,val5,val6,val7,val8,val9,val10);
+	localStorage.setItem(key,value); 
+    window.location.href = "index.html";
+} 
+function toggle10() {
+    
+	var str1="key";
+    var str2=localStorage.length;
+    var key=str1.concat(str2);
+    var val1="<img src='images/busstop.png'>#  #";
+    var val2=document.getElementById('desc').value;
+    var val3="# Latitude: #";
+    var val4=document.getElementById('lat').value;
+    var val5="#, Longitude: #";
+    var val6=document.getElementById('lon').value;
+    var val7="#, Taged Time: #";
+    var currentdate = new Date();
+    var val8 =  currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+    var val9="#images/busstop.png#"; 
+   
+    var val10=document.getElementById('pos').value;
+    var value=val1.concat(val2,val3,val4,val5,val6,val7,val8,val9,val10);
+	localStorage.setItem(key,value); 
+    window.location.href = "index.html";
+} 
+function toggle11() {
+    
+	var str1="key";
+    var str2=localStorage.length;
+    var key=str1.concat(str2);
+    var val1="<img src='images/busstop.png'>#  #";
+    var val2=document.getElementById('desc').value;
+    var val3="# Latitude: #";
+    var val4=document.getElementById('lat').value;
+    var val5="#, Longitude: #";
+    var val6=document.getElementById('lon').value;
+    var val7="#, Taged Time: #";
+    var currentdate = new Date();
+    var val8 =  currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+    var val9="#images/busstop.png#"; 
+   
+    var val10=document.getElementById('pos').value;
+    var value=val1.concat(val2,val3,val4,val5,val6,val7,val8,val9,val10);
+	localStorage.setItem(key,value); 
+    window.location.href = "index.html";
+} 
+  
+ 
+    
+  
+    
+    
